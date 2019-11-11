bc6f0d9b7d70e2e332b93df4ef0215e8c2b8c929
Jason Virtue <jvirtue@microsoft.com>
2019-11-11 14:26:54 -0500

    Add project files.

---
+++ PowerBIEmbedded_AppOwnsData/Models/QnaEmbedConfig.cs
@@ -0,0 +1,10 @@
+using Microsoft.PowerBI.Api.V2.Models;
+using System;
+
+namespace PowerBIEmbedded_AppOwnsData.Models
+{
+    public class QnaEmbedConfig : EmbedConfig
+    {
+        public string datasetId { get; set; }
+    }
+}
